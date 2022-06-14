6445043b8a7a170d8d0f517dc0bbe0a80a3a8fca
Christian Choi <bob1253456@hotmail.com>
2022-06-14 11:40:48 +0800

    init

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA4iu7T4PajESJLIbxobPGYCVp9qhPWipg",
+    authDomain: "tipstore-18610.firebaseapp.com",
+    projectId: "tipstore-18610",
+    storageBucket: "tipstore-18610.appspot.com",
+    messagingSenderId: "224035807098",
+    appId: "1:224035807098:web:79247cd358c01d4d0840c3",
+    measurementId: "G-W72QTYE0WM"
+};
+
+
+export const fire = firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
