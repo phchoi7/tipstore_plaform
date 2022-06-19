07896bafa3fcd6993a33c868a12302f51f517921
Christian Choi <bob1253456@hotmail.com>
2022-06-19 18:28:21 +0800

    ad

--- src/components/AiScoreCard.function.js
+++ src/components/AiScoreCard.function.js
@@ -30,27 +30,27 @@ export function checkName(name) {
 export function returnTeamIcon(name) {
   switch (name) {
     case '巴萨':
-      return '巴塞隆拿';
+      return 'https://sortitoutsi.net/uploads/team/1708.png';
     case '皇马':
-      return '皇家馬德里';
+      return 'https://sortitoutsi.net/uploads/team/1736.png';
     case '西汉姆联':
-      return '韋斯咸';
+      return 'https://sortitoutsi.net/uploads/team/735.png';
     case '切尔西':
-      return '車路士';
+      return 'https://sortitoutsi.net/uploads/team/630.png';
     case '阿森纳':
-      return '阿仙奴';
+      return 'https://sortitoutsi.net/uploads/team/602.png';
     case '尤文图斯':
-      return '祖雲達斯';
+      return 'https://sortitoutsi.net/uploads/team/1139.png';
     case '诺维奇':
-      return '諾域治';
+      return 'https://sortitoutsi.net/uploads/team/691.png';
     case '吉马良斯':
-      return '甘馬雷斯';
+      return 'https://sortitoutsi.net/uploads/team/1494.png';
     case '巴黎圣日尔曼':
-      return '巴黎聖日爾門';
+      return 'https://sortitoutsi.net/uploads/team/868.png';
     case '利兹联':
-      return '列斯聯';
+      return 'https://sortitoutsi.net/uploads/team/671.png';
     case '西班牙人':
-      return '愛斯賓奴';
+      return 'https://sortitoutsi.net/uploads/team/1725.png';
     case '金泉尚武':
       return 'https://sortitoutsi.net/uploads/team/106809.png';
     case '水原城':
@@ -101,6 +101,33 @@ export function returnTeamIcon(name) {
       return 'https://sortitoutsi.net/uploads/team/786384.png';
     case '济州联队':
       return 'https://sortitoutsi.net/uploads/team/106817.png';
+
+    case '水原三星':
+      return 'https://sortitoutsi.net/uploads/team/106813.png';
+    case 'FC首尔':
+      return 'https://sortitoutsi.net/uploads/team/130777.png';
+    case '乌兹别克U23':
+      return 'https://sortitoutsi.net/uploads/flags/144.png';
+    case '沙特U23':
+      return 'https://sortitoutsi.net/uploads/flags/133.png';
+    // havent done
+    case '维京':
+      return 'https://sortitoutsi.net/uploads/team/775187.png';
+    case '桑德菲杰':
+      return 'https://sortitoutsi.net/uploads/team/107303.png';
+    case '特罗姆瑟':
+      return 'https://sortitoutsi.net/uploads/team/107283.png';
+    case '海于格松':
+      return 'https://sortitoutsi.net/uploads/team/7100042.png';
+    case '利勒斯特':
+      return 'https://sortitoutsi.net/uploads/team/786372.png';
+    case '罗森博格':
+      return 'https://sortitoutsi.net/uploads/team/786384.png';
+    case '谢夫':
+      return 'https://sortitoutsi.net/uploads/team/106817.png';
+    case '汉坎':
+      return 'https://sortitoutsi.net/uploads/team/106817.png';
+    // havent done
     default:
       return 'https://www.nepal90.com/images/original/icon_team.png';
   }
